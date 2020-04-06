04094d7a1e2c1b8c6da32a24be80c2f5dffd4ff6
Vegard Sandengen <vegard@orcalabs.no>
2020-04-06 10:25:48 +0200

    build: generate source files in OUT_DIR

--- src/generated/mod.rs
+++ src/generated/mod.rs
@@ -1,6 +1,12 @@
 //! Generated code by build.
 
-pub mod alpha;
-pub mod definition;
+pub mod alpha {
+    include!(concat!(env!("OUT_DIR"), "/src/generated/alpha.rs"));
+}
+pub mod definition {
+    include!(concat!(env!("OUT_DIR"), "/src/generated/definition.rs"));
+}
 #[cfg(feature = "region")]
-pub mod region;
+pub mod region {
+    include!(concat!(env!("OUT_DIR"), "/src/generated/region.rs"));
+}
